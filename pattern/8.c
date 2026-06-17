3e794d257572c7e4c0f012cef7c00cb05f5ced8d
MD. RAJIN SHALEH <u2402052@student.cuet.ac.bd>
2026-06-18 00:02:59 +0600

    Create 8.c

---
+++ pattern/8.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+int m;
+scanf("%d",&m);
+for(int i=1;i<=m;i++){
+for(int j=1;j<=i;j++){
+printf(" ");
+}
+for(int k=1;k<=2*(m-i)+1;k++){
+    printf("*");
+}
+printf("\n");}
+return 0;}
