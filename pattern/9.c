0dd36a332cb0d6d902e8d87ebee8c398fc99c132
MD. RAJIN SHALEH <u2402052@student.cuet.ac.bd>
2026-06-17 20:02:16 +0600

    Create 9.c

---
+++ pattern/9.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main(){
+    int m;
+    scanf("%d",&m);
+    for(int i=1;i<=m;i++){
+        for(int j=1;j<=m+1-i;j++){
+            printf(" ");
+        }
+        for(int k=1;k<=2*i-1;k++){
+            printf("*");
+        }printf("\n");
+    }
+    for(int i=1;i<=m;i++){
+        for(int j=1;j<=i;j++){
+            printf(" ");
+        }
+        for(int k=1;k<=2*(m-i)+1;k++){
+            printf("*");
+        }
+        printf("\n");
+    }
+return 0;}
