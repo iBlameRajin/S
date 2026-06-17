618aaf7886157d1c1bdccc85fa9c4470a5b18d6e
MD. RAJIN SHALEH <u2402052@student.cuet.ac.bd>
2026-06-17 23:38:26 +0600

    Create 20.c

---
+++ pattern/20.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main(){
+    int m;
+    scanf("%d",&m);
+      for(int i=1;i<=m;i++){
+        for(int j=1;j<=i;j++){
+            printf("*");
+        }
+        for(int k=1;k<=2*(m-i);k++)
+            printf(" ");
+
+ for(int l=1;l<=i;l++){
+            printf("*");
+        }
+        printf("\n");
+    }
+    for(int i=2;i<=m;i++){
+        for(int j=1;j<=m+1-i;j++){
+            printf("*");
+        }
+        for(int k=1;k<=2*i-2;k++){
+            printf(" ");
+}
+        for(int l=1;l<=m+1-i;l++){
+            printf("*");
+        }printf("\n");
+    }
+
+return 0;
+}
