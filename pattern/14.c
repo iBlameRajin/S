84e67abd979c0fe62a7d087ec8d68d2cf7823c20
MD. RAJIN SHALEH <u2402052@student.cuet.ac.bd>
2026-06-17 20:31:07 +0600

    Create 14.c

---
+++ pattern/14.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+   int m;
+   scanf("%d",&m);
+   for(int i=1;i<=m;i++){
+    for(int j=1;j<=i;j++){
+        printf("%c",64+j);
+}
+    printf("\n");
+   }
+return 0;}
